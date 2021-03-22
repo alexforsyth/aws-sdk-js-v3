c65570bce1e3c9f3951c9fe67db74398201c3da8
Alex Forsyth <alex-git@amazon.com>
2021-03-22 11:51:06 -0400

    fix: upload browser issue

--- lib/lib-storage/src/runtimeConfig.ts
+++ lib/lib-storage/src/runtimeConfig.ts
@@ -1,9 +1,10 @@
 import { ClientSharedValues } from "./runtimeConfig.shared";
-
+import { lstatSync } from "fs";
 /**
  * @internal
  */
 export const ClientDefaultValues = {
   ...ClientSharedValues,
   runtime: "node",
+  lstatSync,
 };
